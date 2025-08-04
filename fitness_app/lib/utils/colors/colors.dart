9b2d6da5683a562a2efe5994542e9d5d002883c7
Hasiru Randeepa <160104611+haz1ru@users.noreply.github.com>
2025-08-04 14:06:15 +0530

    create dart file for app colors

---
+++ fitness_app/lib/utils/colors/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FitTheme {
+  // Background Colors
+  static const Color backgroundColor = Color(0xFF0A0A0A);
+  
+  // Text Colors
+  static const Color whiteText = Color(0xFFFFFFFF);
+
+  //Button Colors
+  static const Color meroonBut = Color(0xFF6C0B0B);
+  
+}
